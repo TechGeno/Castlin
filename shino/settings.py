b17d6656f8ead6a4ed1473a05f265cf5c53eff41
TechGeno <harshasri910@gmail.com>
2023-10-09 21:29:05 +0530

    csrf allowed changed-3

--- shino/settings.py
+++ shino/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG =config("DEBUG",cast=bool)
 CSRF_TRUSTED_ORIGINS = ['*']
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://castlin-production.up.railway.app/','https://web-production-7494.up.railway.app']
 
 
 # Application definition
